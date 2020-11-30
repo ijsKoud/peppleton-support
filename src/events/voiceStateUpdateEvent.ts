aa4fe51ede852a8bc31cf935b727f1cff8e44930
DaangamesDG <dwklarenbeek@gmail.com>
2020-11-30 19:52:30 +0100

    fix for wrong var oop

--- src/events/voiceStateUpdateEvent.ts
+++ src/events/voiceStateUpdateEvent.ts
@@ -1,4 +1,4 @@
-import { eventsChannel, eventsVoiceChannel } from '../../config/config';
+import { eventsTextChannel, eventsVoiceChannel } from '../../config/config';
 import { VoiceState, TextChannel } from 'discord.js';
 import BaseEvent from '../utils/structures/BaseEvent';
 import DiscordClient from '../client/client';
@@ -13,16 +13,20 @@ export default class voiceStateUpdateEvent extends BaseEvent {
       (oldState.channel && !newState.channel) 
       && (oldState.channelID === eventsVoiceChannel 
         && newState.channelID !== eventsVoiceChannel)
-    ) return !client.channels.cache.has(eventsChannel) ? 
-      (await client.channels.fetch(eventsChannel, true, true) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: false })
-      : (client.channels.cache.get(eventsChannel) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: false });
+    ) {
+      !client.channels.cache.has(eventsTextChannel) ? 
+      (await client.channels.fetch(eventsTextChannel, true, true) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: false })
+      : (client.channels.cache.get(eventsTextChannel) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: false });
+    }
 
     if (
       (!oldState.channel && newState.channel) 
       && (oldState.channelID !== eventsVoiceChannel 
         && newState.channelID == eventsVoiceChannel)
-    ) return !client.channels.cache.has(eventsChannel) ? 
-    (await client.channels.fetch(eventsChannel, true, true) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: true })
-    : (client.channels.cache.get(eventsChannel) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: true });
+    ) {
+      !client.channels.cache.has(eventsTextChannel) ? 
+      (await client.channels.fetch(eventsTextChannel, true, true) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: true })
+      : (client.channels.cache.get(eventsTextChannel) as TextChannel).updateOverwrite(oldState.member, { VIEW_CHANNEL: true });
+    }
   }
 }
