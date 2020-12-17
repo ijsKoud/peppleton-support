518e9c35f19f1816f1b56b05386e37b2199aa2b6
DaangamesDG <dwklarenbeek@gmail.com>
2020-12-17 20:54:44 +0100

    added active tickets cache on ready

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -1,6 +1,7 @@
 import { guildId } from '../../../config/config';
 import BaseEvent from '../../utils/structures/BaseEvent';
 import DiscordClient from '../../client/client';
+import { TextChannel } from "discord.js";
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
@@ -10,6 +11,9 @@ export default class ReadyEvent extends BaseEvent {
     const guild = await client.guilds.fetch(guildId);
     const valid = guild.channels.cache.filter(c => c.name.endsWith('-ticket'));
     valid.forEach(c => client.openTickets.set(c.name.slice(0, -7), true));
+    valid.forEach(c => 
+      client.activeTickets.set(c.id, { reason: "", lastMsg: (c as TextChannel).lastMessage.createdTimestamp })
+    );
 
     setInterval(() => {
       client.activeTickets.forEach(async (v, k) => {
