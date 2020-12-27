1565f1ea99a98b97853a8829317b68d019a6956e
DaangamesDG <dwklarenbeek@gmail.com>
2020-12-27 10:50:02 +0100

    fixed ticket close issue

--- config/config.ts
+++ config/config.ts
@@ -1,29 +1,30 @@
 // real config file -> checl config_dev.ts for the dev version
 
-export const eventsVoiceChannel: string =  '748140133827739667';
-export const eventsTextChannel: string = '751893103031615640';
-export const eventNotification: string = '739489234892750919';
-export const reactionRoleMSG: string = '777295875671064577';
-export const reactionEmojiName: string = '🔔';
+export const eventsVoiceChannel: string = "748140133827739667";
+export const eventsTextChannel: string = "751893103031615640";
+export const eventNotification: string = "739489234892750919";
+export const reactionRoleMSG: string = "777295875671064577";
+export const reactionEmojiName: string = "🔔";
 
-export const eventsChannel: string = '751889989171806259';
+export const eventsChannel: string = "751889989171806259";
 
-export const feedbackSheet: string = '1r8a--e9-THYU_w8ql6Qlg_xxVkag_trKL0DhudRonSg';
-export const feedbackMsgId: string = '786684201247571978';
+export const feedbackSheet: string =
+	"1r8a--e9-THYU_w8ql6Qlg_xxVkag_trKL0DhudRonSg";
+export const feedbackMsgId: string = "786684201247571978";
 
-export const qdEmoji: string = '<:QD:792118596347756604>';
-export const dsEmoji: string = '<:DS:792118595068231701>';
-export const gdEmoji: string = '<:GD:792118596205412392>';
-export const prEmoji: string = '<:PRLogo:771799330516107265>';
+export const qdEmoji: string = "<:QD:792118596347756604>";
+export const dsEmoji: string = "<:DP:792499533384384522>";
+export const gdEmoji: string = "<:GD:792118596205412392>";
+export const prEmoji: string = "<:PRLogo:771799330516107265>";
 
-export const blacklistRole: string = '781589052197568512';
+export const blacklistRole: string = "781589052197568512";
 
-export const guildId: string = '705523495563427872';
-export const categoryId: string = '754266502638600233';
+export const guildId: string = "705523495563427872";
+export const categoryId: string = "754266502638600233";
 
-export const qdDepChannel: string = '780500433504043039';
-export const dsDepChannel: string = '780500462218379304';
-export const gdDepChannel: string = '780500873544597534';
-export const anyDepChannel: string = '780500364448235530';
+export const qdDepChannel: string = "780500433504043039";
+export const dsDepChannel: string = "780500462218379304";
+export const gdDepChannel: string = "780500873544597534";
+export const anyDepChannel: string = "780500364448235530";
 
 export const suggestionsChannel: string = "788811272899919872";
--- src/commands/Tickets/CloseCommand.ts
+++ src/commands/Tickets/CloseCommand.ts
@@ -1,32 +1,40 @@
-import { Message, TextChannel } from 'discord.js';
-import BaseCommand from '../../utils/structures/BaseCommand';
-import DiscordClient from '../../client/client';
+import { Message, TextChannel } from "discord.js";
+import BaseCommand from "../../utils/structures/BaseCommand";
+import DiscordClient from "../../client/client";
 
 export default class CloseCommand extends BaseCommand {
-  constructor() {
-    super('close', 'Tickets', [], {
-      description: 'Closes a ticket, very simple right?',
-      usage: '',
-      ownerOnly: false,
-    });
-  }
+	constructor() {
+		super("close", "Tickets", [], {
+			description: "Closes a ticket, very simple right?",
+			usage: "",
+			ownerOnly: false,
+		});
+	}
 
-  async run(client: DiscordClient, message: Message, args: Array<string>) {
-    if (message.channel.type == 'dm') return message.channel.send(
-      `> ❌ | This command is only usable in a ticket channel. If you wish to close a ticket, ask the ticket claimer to close it.`
-    );
+	async run(client: DiscordClient, message: Message, args: Array<string>) {
+		if (message.channel.type == "dm")
+			return message.channel.send(
+				"> ❌ | This command is only usable in a ticket channel. If you wish to close a ticket, ask the ticket claimer to close it."
+			);
 
-    const channel = message.channel as TextChannel;
-    if (!channel.name.endsWith('-ticket')) return message.channel.send(
-      `> ❌ | This command is only usable in a ticket channel.`
-    );
-    if (!channel.topic.includes(message.author.id)) return message.channel.send(
-      `> ❌ | Only ticket owners can execute this command.`
-    );
+		const channel = message.channel as TextChannel;
+		if (!channel.name.endsWith("-ticket"))
+			return message.channel.send(
+				"> ❌ | This command is only usable in a ticket channel."
+			);
+		if (
+			!channel.topic.includes(message.author.id) &&
+			!message.member.hasPermission("MANAGE_GUILD")
+		)
+			return message.channel.send(
+				"> ❌ | Only ticket owners can execute this command."
+			);
 
-    try {
-      channel.delete();
-      return message.author.send(`> ✅ | Successfully closed: **${channel.name}**.`);
-    } catch (e) { }
-  }
+		try {
+			channel.delete();
+			return message.author.send(
+				`> ✅ | Successfully closed: **${channel.name}**.`
+			);
+		} catch (e) {}
+	}
 }
