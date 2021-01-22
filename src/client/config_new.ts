ca4033213ed04e841d1e625b80846f6a882b2060
DaangamesDG <dwklarenbeek@gmail.com>
2021-01-22 16:02:16 +0100

    added comments to config

--- src/client/config_new.ts
+++ src/client/config_new.ts
@@ -1,43 +1,54 @@
+// client config
 export const owners: string[] = ["304986851310043136"];
 export const prefix: string = "=";
 
+// tokens
 export const token: string = "NzExNDY4ODkzNDU3MDg4NTUz.XsDdCg.1pPxf6o6m4pR-bcklXsCUnXrChg";
 export const db: string =
 	"mongodb+srv://peppleton-support:Tvvq9tUSoFbLmRL4@peppleton-support.ipvxi.mongodb.net/peppleton?retryWrites=true&w=majority";
 export const googleApi: string = "AIzaSyDNMlSnrN6G6_DxQwbZgvuWq9wHNPkMwwM";
 
+// webhook
 export const wbId: string = "791646258515148801";
 export const wbToken: string =
 	"fMnLDIjZT-BgWUDnxySPS1nHtS2pfgonKSTzeAfO1XPPcXxZmE1GafBMyDeBPUmN4QBB";
 
+// events
 export const eventsVoiceChannel: string = "748140133827739667";
 export const eventsTextChannel: string = "751893103031615640";
 export const eventNotification: string = "739489234892750919";
-export const reactionRoleMSG: string = "798112481448034324";
+export const eventsChannel: string = "751889989171806259";
 export const eventsTeam: string = "751887409255546901";
-export const reactionEmojiName: string = "🔔";
-export const reactionEmojiName2: string = "🗓️";
 
+// QOTD
 export const QOTDChannel: string = "797618660188880927";
 export const QOTDNotifications: string = "797902390208364594";
 
-export const eventsChannel: string = "751889989171806259";
+// reaction roles
+export const reactionRoleMSG: string = "798112481448034324";
+export const reactionEmojiName: string = "🔔";
+export const reactionEmojiName2: string = "🗓️";
 
+// feedback
 export const feedbackSheet: string = "1r8a--e9-THYU_w8ql6Qlg_xxVkag_trKL0DhudRonSg";
 export const feedbackMsgId: string = "786684201247571978";
 
+// emojis
 export const qdEmoji: string = "<:Driver:793907766476668938>";
 export const dsEmoji: string = "<:Dispatcher:793907746390933534>";
 export const gdEmoji: string = "<:Guard:793907789349126214>";
 export const prEmoji: string = "<:PRLogo:771799330516107265>";
 
+// roles
 export const blacklistRole: string = "781589052197568512";
 export const svRole: string = "742790430034362440";
 export const mRole: string = "742791627495571596";
 
+// required ids
 export const guildId: string = "705523495563427872";
 export const categoryId: string = "754266502638600233";
 
+/// ticket departments
 export const qdDepChannel: string = "780500433504043039";
 export const dsDepChannel: string = "780500462218379304";
 export const gdDepChannel: string = "780500873544597534";
@@ -46,9 +57,11 @@ export const managerChannel: string = "792914866574589952";
 export const devChannel: string = "792914883284172841";
 export const dirChannel: string = "792914904784175185";
 
+// extra channel ids
 export const suggestionsChannel: string = "788811272899919872";
 export const transcriptionsChannel: string = "793203715154378752";
 
+// reports
 export const DrivingReports: string = "798639223817109525";
 export const DispatchReports: string = "798639351012655124";
 export const guardReports: string = "798639473373085758";
