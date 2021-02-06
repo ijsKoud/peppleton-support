eec3b4a8941fe578cf2a1c9096d49e505d9c3e28
DaangamesDG <dwklarenbeek@gmail.com>
2021-02-06 12:13:57 +0100

    added new transcript system

--- src/commands/tickets/closeCommand.ts
+++ src/commands/tickets/closeCommand.ts
@@ -1,5 +1,8 @@
+import { exec } from "child_process";
 import { Command } from "discord-akairo";
-import { Message } from "discord.js";
+import { Message, MessageAttachment, TextChannel } from "discord.js";
+import { join } from "path";
+import { transcriptionsChannel } from "../../client/config";
 
 export default class closeCommand extends Command {
 	public constructor() {
@@ -16,7 +19,7 @@ export default class closeCommand extends Command {
 		});
 	}
 
-	public exec(message: Message) {
+	async exec(message: Message) {
 		if (message.channel.type !== "text" || !message.channel.name.endsWith("-ticket")) return;
 
 		let allowed: boolean = false;
@@ -27,7 +30,41 @@ export default class closeCommand extends Command {
 
 		if (!allowed) return message.react("❌");
 
-		message.util.send(`>>> 🗑 | Deleting the channel in **5 seconds**...`);
-		setTimeout(() => message.channel.delete("closed by claimer"), 5e3);
+		exec(
+			`DiscordChatExporter.Cli.exe export -c ${message.channel.id} -t ${
+				this.client.token
+			} -o ${join(__dirname, "..", "..", "transcriptions")} -b`,
+			{
+				cwd: join(process.cwd(), "transcriptor"),
+			},
+			async (e, m) => {
+				if (e) this.client.log(`Transcript error: ${e}`);
+				try {
+					const tsChannel = (await this.client.channels.fetch(
+						transcriptionsChannel
+					)) as TextChannel;
+					tsChannel.send(
+						new MessageAttachment(
+							join(
+								__dirname,
+								"..",
+								"..",
+								"..",
+								"transcriptions",
+								`${message.guild.name} - ${
+									(message.channel as TextChannel).parent?.name || "text"
+								} - ${(message.channel as TextChannel).name} [${message.channel.id}].html`
+							),
+							`${message.channel.id}-ticket.html`
+						)
+					);
+				} catch (e) {
+					this.client.log(`Transcript error: ${e}`);
+				}
+
+				message.util.send(`>>> 🗑 | Deleting the channel in **5 seconds**...`);
+				setTimeout(() => message.channel.delete("closed by claimer"), 5e3);
+			}
+		);
 	}
 }
