30ac82ebcf530ced296fe14384b6f60ce569de0c
DaangamesDG <dwklarenbeek@gmail.com>
2021-02-26 14:28:50 +0100

    utils update

--- src/client/util.ts
+++ src/client/util.ts
@@ -52,7 +52,10 @@ export default class util {
 	public async fetchUser(id: string): Promise<User> {
 		let user: User = null;
 
-		if (!isNaN(Number(id))) user = await this.client.users.fetch(id, true).catch((e) => null);
+		if (!isNaN(Number(id)))
+			user =
+				this.client.users.cache.get(id) ||
+				(await this.client.users.fetch(id, true).catch((e) => null));
 		else user = this.client.util.resolveUser(id, this.client.users.cache, false, false);
 
 		return user || null;
