d19d2b896a07bde585f34377cce7acf44ed6d48d
DaanGamesDG <65551719+DaanGamesDG@users.noreply.github.com>
2021-04-18 12:23:35 +0200

    Fixing pings

--- src/events/message/message.ts
+++ src/events/message/message.ts
@@ -12,6 +12,7 @@ export default class message extends Listener {
 	async exec(message: Message) {
 		try {
 			if (message.author.bot || message.system || message.webhookID) return;
+			await this.pings(message);
 
 			if (
 				/<((@!?\d+)|(:.+?:\d+))>/g.test(message.content.trim().split(/ +/g).shift()) &&
