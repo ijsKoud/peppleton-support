d150947d8b1d9ffcdc98972fe9ddb59e9c47fd24
MarcusPlays1 <76704801+MarcusPlays1@users.noreply.github.com>
2021-07-19 21:53:05 +0100

    i forgot to update id

--- src/commands/General/messagesCommand.ts
+++ src/commands/General/messagesCommand.ts
@@ -24,7 +24,7 @@ export default class messagesCommand extends Command {
 	async exec(message: Message, { id }: { id: string }) {
 		if (!["866700533594062859", "803013913150488617"].includes(message.channel.id))
 			return message.util.send(
-				`>>> ${this.client.mocks.emojis.redcross} | You can only use this command in <#803013913150488617> or <#721360723351044149>!`
+				`>>> ${this.client.mocks.emojis.redcross} | You can only use this command in <#803013913150488617> or <#866700533594062859>!`
 			);
 
 		let user = await this.client.utils.fetchUser(id);
