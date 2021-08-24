32946b743b19cebefb367a70fc142e7be1af5055
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 17:49:29 +0200

    Feat(MessageCreate): Added command block

--- src/bot/listeners/message/MessageCreate.ts
+++ src/bot/listeners/message/MessageCreate.ts
@@ -5,6 +5,7 @@ import { Message } from "discord.js";
 @ApplyOptions<ListenerOptions>({ once: false, event: "messageCreate" })
 export default class MessageCreateListener extends Listener {
 	public async run(message: Message) {
+		if (message.author.bot || message.system || message.webhookId) return;
 		if (
 			/<((@!?\d+)|(:.+?:\d+))>/g.test(message.content.trim().split(/ +/g).shift() ?? "") &&
 			message.mentions.users.has(this.container.client.user?.id ?? "") &&
@@ -13,5 +14,10 @@ export default class MessageCreateListener extends Listener {
 			return this.container.client.supportHandler.handleMention(message);
 
 		this.container.client.supportHandler.ticketHandler.handleMessages(message);
+		if (
+			message.guildId &&
+			!message.content.startsWith(this.container.client.options.defaultPrefix?.toString() ?? "!")
+		)
+			this.container.client.activityManager.update(message.author.id, message.guildId);
 	}
 }
