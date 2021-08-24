a1c22f3c1ab25ff1cac9175f2cf6c2e1d029ca7e
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 15:42:54 +0200

    Added LoungeVoiceStateUpdate

---
+++ src/bot/listeners/voice/LoungeVoiceStateUpdate.ts
@@ -0,0 +1,37 @@
+import { Listener, ListenerOptions } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { TextChannel, VoiceState } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({ once: true, event: "voiceStateUpdate" })
+export default class LoungeVoiceStateUpdateListener extends Listener {
+	public async run(oldState: VoiceState, newState: VoiceState): Promise<void> {
+		const { client } = this.container;
+		const { GvoiceChannel, GtextChannel } = client.constants.voice;
+		const { manager, botDev } = client.constants.departments;
+
+		if (!oldState.member) return;
+		if (oldState.member.partial) await oldState.member.fetch();
+
+		if (oldState.member.roles.cache.has(botDev) || oldState.member.roles.cache.has(manager)) return;
+		if (newState.channelId !== GvoiceChannel && oldState.channelId === GvoiceChannel)
+			!client.channels.cache.has(GtextChannel)
+				? ((await client.channels.fetch(GtextChannel)) as TextChannel).permissionOverwrites.create(
+						oldState.member,
+						{ VIEW_CHANNEL: false }
+				  )
+				: (client.channels.cache.get(GtextChannel) as TextChannel).permissionOverwrites.create(
+						oldState.member,
+						{ VIEW_CHANNEL: false }
+				  );
+		else if (newState.channelId === GvoiceChannel && oldState.channelId !== GvoiceChannel)
+			!client.channels.cache.has(GtextChannel)
+				? ((await client.channels.fetch(GtextChannel)) as TextChannel).permissionOverwrites.create(
+						oldState.member,
+						{ VIEW_CHANNEL: true }
+				  )
+				: (client.channels.cache.get(GtextChannel) as TextChannel).permissionOverwrites.create(
+						oldState.member,
+						{ VIEW_CHANNEL: true }
+				  );
+	}
+}
