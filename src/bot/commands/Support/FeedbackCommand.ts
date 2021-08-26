25eb5b2c89f6ec3a121b978da65e540bece8ccdd
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-26 19:52:49 +0200

    Added FeedbackCommand

---
+++ src/bot/commands/Support/FeedbackCommand.ts
@@ -0,0 +1,24 @@
+import { Command } from "../../../client/structures/Command";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Message } from "discord.js";
+import { Args } from "@sapphire/framework";
+
+@ApplyOptions<Command.Options>({
+	name: "feeback",
+	aliases: ["feeback"],
+	description: "Changes the feedback message id",
+	preconditions: ["ManagerOnly", "GuildOnly"],
+	usage: "[id]",
+})
+export default class FeedbackCommand extends Command {
+	public async run(message: Message, args: Args) {
+		const { client } = this.container;
+		const { value: id } = await args.pickResult("string");
+
+		await client.prisma.feedback.update({
+			where: { guildId: message.guildId ?? "" },
+			data: { messageId: id ?? "" },
+		});
+		return message.react("✅");
+	}
+}
