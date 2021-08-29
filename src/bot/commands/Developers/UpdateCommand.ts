c128da50676553dc31e195148f6ca0d72eac401a
MarcusNawDev <88289190+MarcusNawDev@users.noreply.github.com>
2021-08-29 14:28:31 +0100

    fixing my ocd

--- src/bot/commands/Developers/UpdateCommand.ts
+++ src/bot/commands/Developers/UpdateCommand.ts
@@ -24,17 +24,17 @@ export default class UpdateCommand extends Command {
 
 		const msg = await message.reply(">>> 🤖 | Update Command**");
 		if (fetch) {
-			await msg.edit(">>> 🤖 | Update Command**\nFetching code from GitHub");
+			await msg.edit(">>> 🤖 | **Update Command**\nFetching code from GitHub");
 			await this.Exec("git pull");
 		}
 
 		if (update) {
-			await msg.edit(">>> 🤖 | Update Command**\nInstalling new Dependencies");
+			await msg.edit(">>> 🤖 | **Update Command**\nInstalling new Dependencies");
 			await this.Exec("yarn install");
 		}
 
 		if (restart) {
-			await msg.edit(">>> 🤖 | Update Command**\nBot is updated - restarting...");
+			await msg.edit(">>> 🤖 | **Update Command**\nBot is updated - restarting...");
 			return process.exit(0);
 		}
 
