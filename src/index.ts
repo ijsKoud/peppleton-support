20701a97e8b35b2f4dfa02fa6494d677241d2816
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-31 15:02:10 +0100

    chore: update client config

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ config();
 import { Client } from "./client";
 
 const client = new Client({
-	intents: ["GUILDS", "GUILD_MESSAGES"],
-	partials: ["MESSAGE"],
+	intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "DIRECT_MESSAGES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"],
+	partials: ["MESSAGE", "CHANNEL", "GUILD_MEMBER", "REACTION"],
 	owners: (process.env.OWNERS ?? "").split(","),
 	activity: [
 		{
 			type: "LISTENING",
-			name: "DaanGamesDG"
+			name: "your support tickets"
 		}
 	]
 });
